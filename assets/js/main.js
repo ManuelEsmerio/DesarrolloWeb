ff21f3c1a638a4817c18aab77baa8bf579ff6f38
Manuel Esmerio Garcia <manuel.esmerio1995@gmail.com>
2021-09-02 15:37:55 -0500

    4ta Entrega del proyecto final

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,14 @@ menu_btn.addEventListener("click", () => {
 	sidebar.classList.toggle("active-nav");
 	container.classList.toggle("active-cont");
 });
+
+window.addEventListener("resize", function (event) {
+	if (document.body.clientWidth <= 495) {
+		sidebar.classList.remove("active-nav");
+		container.classList.remove("active-cont");
+	}
+	else if (document.body.clientWidth > 495) {
+		sidebar.classList.add("active-nav");
+		container.classList.add("active-cont");
+	}
+})
